a6424ecff01903f3ef37721d6c2c884f60ab0094
吕泷 <35415036+nonamehehe@users.noreply.github.com>
2020-11-04 22:07:13 +0800

    add

--- 基础操作/lambda_map等操作区别.py
+++ 基础操作/lambda_map等操作区别.py
@@ -2,14 +2,14 @@
 lambda argument_list：expression
 1、lambda x, y: x*y     函数输入是x和y，输出是它们的积x*y
 add=lambda x, y: x+y
-add(1,2)=3
+add(1,2)  #add(1,2)=3
 
 二、将lambda函数作为参数传递给其他函数
 
 找出3的倍数：
 1、filter（）函数：接收两个参数，一个是函数，一个是序列。将序列中的元素依次传入函数，计算后为返回为True的序列。
->>> list(filter(lambda x: x % 3 == 0, [1, 2, 3]))
-[3]
+list(filter(lambda x: x % 3 == 0, [1, 2, 3]))
+输出=[3]
 
 2、map函数：接收两个参数，一个是函数，一个是序列。将序列中的元素依次传入函数，计算后为返回新的序列。
 >>> list(map(lambda x: x+1, [1, 2,3]))
